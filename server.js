1478894821d90e1c76a56a657429d08e5c0de04f
Digital Skills House <162470282+DigitalSkillsHouse@users.noreply.github.com>
2025-11-29 14:02:04 +0500

    update server.js file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+
 const { createServer } = require("http");
 const next = require("next");
 
-// Detect production correctly (cPanel sets NODE_ENV=production)
+// Railway ALWAYS provides PORT → never set default 3000
+const port = process.env.PORT;
 const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -13,6 +14,30 @@ app.prepare().then(() => {
     handle(req, res);
   }).listen(port, "0.0.0.0", (err) => {
     if (err) throw err;
-    console.log(`Server is ready on port ${port}`);
+    console.log(`🚀 Server running on port ${port}`);
   });
 });
+
+
+
+
+
+
+// const { createServer } = require("http");
+// const next = require("next");
+
+// // Detect production correctly (cPanel sets NODE_ENV=production)
+// const dev = process.env.NODE_ENV !== "production";
+// const port = process.env.PORT || 3000;
+
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     handle(req, res);
+//   }).listen(port, "0.0.0.0", (err) => {
+//     if (err) throw err;
+//     console.log(`Server is ready on port ${port}`);
+//   });
+// });
